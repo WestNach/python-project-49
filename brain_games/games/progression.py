202e3adeab38513f5fb58a35feae3d4fdaa58bd0
Чекменёв Владислав <nahvest@gmail.com>
2023-09-30 17:10:04 +0300

    cut function in progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,11 +2,14 @@ import random
 RULE_GAME = 'What number is missing in the progression?'
 
 
-def variables():
+def generate_data():
     start = random.randint(0, 10)
     diff = random.randint(1, 10)
     length = random.randint(5, 10)
     miss_index = random.randint(0, length - 1)
+    return start, diff, length, miss_index
+
+def make_quest(start,diff,length,miss_index):
     Quest = []
     ind = 0
     for ind in range(length):
@@ -15,5 +18,9 @@ def variables():
             Quest.append("..")
         else:
             Quest.append(start + ind * diff)
+        return Quest, true_answ
+
+
+def variables(Quest, true_answ):
     Quest = " ".join(map(str, Quest))
     return Quest, str(true_answ)
