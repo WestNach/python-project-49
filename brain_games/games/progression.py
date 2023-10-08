debaeb40228608d4d6b11d6d936804f052c5e7ef
Чекменёв Владислав <nahvest@gmail.com>
2023-10-08 18:45:27 +0300

    fix progressive function question_ansver()

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,16 +2,6 @@ import random
 RULE = 'What number is missing in the progression?'
 
 
-def question_answer():
-    miss_index = generate_data.miss_index
-    progression = create_progression.progression
-    create_quest = list(progression)
-    create_true_answ = progression[miss_index]
-    create_quest[miss_index] = ".."
-    quest = " ".join(map(str, create_quest))
-    true_answ = str(create_true_answ)
-    return quest, true_answ
-
 def generate_data():
     index_correction = 1
     min_value = 0
@@ -23,9 +13,18 @@ def generate_data():
     miss_index = random.randint(min_value, length - index_correction)
     return initial_term, common_difference, length, miss_index
 
-
 def create_progression(initial_term, common_difference, length):
     progression = []
     for i in range(length):
         progression.append(initial_term + i * common_difference)
     return progression
+
+def question_answer():
+    initial_term, common_difference, length, miss_index = generate_data()
+    progression = create_progression(initial_term, common_difference, length)
+    create_quest = list(progression)
+    create_true_answ = progression[miss_index]
+    create_quest[miss_index] = ".."
+    quest = " ".join(map(str, create_quest))
+    true_answ = str(create_true_answ)
+    return quest, true_answ
